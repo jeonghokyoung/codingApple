ba56ceedfafe03d695823e629dcdec33bd695111
lucy <mikapuking@gmail.com>
2021-11-13 01:09:24 +0900

    first commit

--- spread_operator.js
+++ spread_operator.js
@@ -45,4 +45,4 @@
 // }
 
 // person.인사.apply(person2, [1,2])
-// person.인사.call(person2, 1, 2) // apply call 비슷함 근데 apply는 파라미터를 array형태로 집어넣기 가능
+// person.인사.call(person2, 1, 2) // apply call 비슷함 근데 apply는 파라미터를 array형태로 집어넣기 가능 
